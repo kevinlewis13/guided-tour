65d412af7ae02df851d5180fd5c94689b4a6ff95
Kevin Lewis <kevinlewis13@gmail.com>
2015-06-15 11:08:34 -0700

    creates server file

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var express = require('express');
+
+var app = express();
+
+var toursRouter = express.Router();
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/tours_development');
+
+app.use(express.static(__dirname + '/build'));
+
+require('./backend/routes/tours_routes')(toursRouter);
+
+app.use('/api', toursRouter);
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('server running on port ' + (process.env.PORT || 3000));
+});
