a9987d04293ee8c8ab1c21d41683b99ef5fd33b5
Kevin Lewis <kevinlewis13@gmail.com>
2015-06-18 14:24:05 -0700

    still messing with directive

--- app/js/directives/waypoint_info_directive.js
+++ app/js/directives/waypoint_info_directive.js
@@ -6,7 +6,9 @@ module.exports = function(app) {
       restrict: 'A',
       replace: true,
       templateUrl: '/templates/directives/waypoint_info.html',
-      scope: false,
+      scope: {
+        currentWaypoint: '@'
+      },
       transclude: true //not sure if we'll need this, but if we do I'd rather it start breaking things early on
     };
   });
